bace285e9a5a500651700ca32e437a9b98e216c2
Daniel Noble <daniel.wa.noble@gmail.com>
2019-04-08 16:52:56 +1000

    Added cleasby data, but need RDA ad stuff

---
+++ data-raw/dat.cleasby2012.r
@@ -0,0 +1,8 @@
+dat.cleasby2012.1 <- read.csv("dat.cleasby2012.1.csv")
+
+save(dat.cleasby2012.1, file="../data/dat.cleasby2012.1.rda", ascii=FALSE)
+
+dat.cleasby2012.2 <- read.csv("dat.cleasby2012.2.csv")
+
+save(dat.cleasby2012.2, file="../data/dat.cleasby2012.2.rda", ascii=FALSE)
+
