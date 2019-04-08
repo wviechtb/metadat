21131239532c89679c4f5a84f1901e8a891b7b30
ekothe <pc.miss@gmail.com>
2019-04-08 14:48:25 +1000

    Add Valstad, M., Alvares, G. A., Egknud, M., Matziorinis, A. M., Andreassen, O. A., Westlye, L. T., & Quintana, D. S. (2017). The correlation between central and peripheral oxytocin concentrations: a systematic review and meta-analysis. Neuroscience & Biobehavioral Reviews, 78, 117-124.

---
+++ data-raw/dat.valstad2017.r
@@ -0,0 +1,9 @@
+dat.valstad2017 <- read.csv("dat.valstad2017.csv", header=TRUE, stringsAsFactors=FALSE)
+
+dat.valstad2017$id <- dat.valstad2017$X
+dat.valstad2017$X <- NULL
+dat.valstad2017$es.id <- NULL
+
+dat.valstad2017 <- dat.valstad2017[,c(12, 1:11)]
+
+save(dat.valstad2017, file="../data/dat.valstad2017.rda", ascii=FALSE)
