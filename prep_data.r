442c5e313a1f3d04a8ed1ed86ee223a4557f8001
Wolfgang Viechtbauer <wvb@wvbauer.com>
2019-04-08 06:15:48 +0200

    Moved prep_data.r to root.

--- prep_data.r
+++ prep_data.r
@@ -1,7 +1,11 @@
-rfiles <- list.files(pattern="[.r]$")
+rfiles <- list.files(path="data-raw", pattern="[.r]$")
 rfiles <- rfiles[rfiles != "prep_data.r"]
-rfiles
+#rfiles
+
+setwd("data-raw")
 
 for (i in 1:length(rfiles)) {
    source(rfiles[i])
 }
+
+setwd("..")
