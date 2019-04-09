167a302f0f85f710bae455fe5b02ca22f060a3dd
Wolfgang Viechtbauer <wvb@wvbauer.com>
2019-04-09 04:42:30 +0200

    Updated prep_data.r script.

--- prep_data.r
+++ prep_data.r
@@ -1,40 +1,77 @@
 prep_data <- function(rebuild=FALSE, overwrite=NULL) {
 
-   ### list all .r/.R files in the 'data-raw' directory
-   rfiles <- list.files(path="data-raw", pattern="[.][rR]$")
-   #rfiles <- rfiles[rfiles != "prep_data.r"]
-   #rfiles
+   ### load .rfiles.txt (that already exist)
+   .rfiles <- read.table("data-raw/.rfiles.txt", header=FALSE, as.is=TRUE)[[1]]
 
-   cat("File", paste0(rep(" ", max(nchar(rfiles))-4), collapse=""), "New", "Build", "\n")
-   cat(paste0(rep("-", max(nchar(rfiles))), collapse=""), "-----------", "\n", sep="")
+   ### get names of all data preparation scripts (.r/.R files) in the 'data-raw' directory
+   rfiles <- list.files(path="data-raw", pattern=".[rR]$")
 
-   ### source data preparation scripts
+   ### paste header for output
+   cat("File", paste0(rep(" ", max(nchar(rfiles))-4), collapse=""), "new ", "rda ", "build ", "Rd ", "create", "\n")
+   cat(paste0(rep("-", max(nchar(rfiles))+29), collapse=""), "\n")
+
+   ### process all data preparation scripts
+
    for (i in seq_along(rfiles)) {
 
-      ### get 'root' name of the data preparation script
+      ### paste name of the data preparation script (and enough space to align everything)
+      cat(rfiles[i], paste0(rep(" ", max(nchar(rfiles))-nchar(rfiles[i])+1), collapse=""))
+
+      ### check if the data preparation file is already in .rfile.txt
+      rfile.exists <- rfiles[i] %in% .rfiles
+
+      ### [New]: paste T/F if it is a new script or not
+      cat(ifelse(!rfile.exists, "T    ", "F    "))
+
+      ### get 'root' name of the data preparation script (remove .r/.R extension)
       root <- substr(rfiles[i], 1, nchar(rfiles[i])-2)
 
-      ### list all .rda files in the 'data' directory
-      ### TODO: do we need to do this within the loop?
-      rda.files <- list.files(path="data")
+      ### get names of all .rda files in the 'data' directory (do we need to do this within the loop?)
+      rda.files <- list.files(path="data", pattern=".rda$")
 
-      ### grep for all root.<anything>.rda files
+      ### grep names of all <root>.<anything>.rda files (could be one or multiple)
       rda.files <- grep(paste0("^", root, ".*.rda"), rda.files, value=TRUE)
 
-      ### check if .rda file(s) exists
-      rda.exists <- ifelse(length(rda.files) > 0, TRUE, FALSE)
+      ### check if <root>.<anything>.rda files already exist
+      rda.files.exist <- ifelse(length(rda.files) > 0, TRUE, FALSE)
 
-      cat(rfiles[i], paste0(rep(" ", max(nchar(rfiles))-nchar(rfiles[i])), collapse=""), ifelse(!rda.exists, "T   ", "F   "))
+      ### [rda]: paste T/F if the rda files already exist or not
+      cat(ifelse(rda.files.exist, "T    ", "F    "))
 
-      ### if rebuild=TRUE or if file does not exist, process data preparation script
-      if (rebuild || !rda.exists) {
-         cat("T")
+      ### if rebuild=TRUE or if the rda files do not exist, process the data preparation script
+      ### [Build]: paste T/F if the data processing script is run
+      if (rebuild || !rda.files.exist) {
+         cat("T      ")
          source(rfiles[i])
       } else {
-         cat("F")
+         cat("F      ")
       }
 
-      #genrd(root, rda.files)
+      ### list all files in the 'data' directory (possibly includes non-.rda files)
+      data.files <- list.files(path="data")
+
+      ### check if there are now any non-.rda files in 'data'; if so, throw an error
+      if (any(tools::file_ext(data.files) != "rda"))
+         stop("\n\nThere are non-.rda files in the 'data' directory.\nData preparation scripts must create only .rda files.")
+
+      ### check if <root>.Rd file exists in 'man' directory
+      rd.exists <- file.exists(paste0("man/", root, ".Rd"))
+
+      ### [Rd]: paste T/F if <root>.Rd file exists or not
+      if (rd.exists) {
+         cat("T   ")
+      } else {
+         cat("F   ")
+      }
+
+      ### if it doesn't exist or if it is part of 'overwrite', create template <root>.Rd file
+      ### [Create]: paste T/F if template <root>.Rd file is created
+      if (!rd.exists || paste0(root, ".Rd") %in% overwrite) {
+         cat("T      ")
+         #genrd(root, rda.files)
+      } else {
+         cat("F      ")
+      }
 
       cat("\n")
 
@@ -45,11 +82,13 @@ prep_data <- function(rebuild=FALSE, overwrite=NULL) {
    #
    #}
 
+   write.table(rfiles, file="data-raw/.rfiles.txt", row.names=FALSE, col.names=FALSE)
+
 }
 
 ### this is just for testing purposes; ignore
 
-if (F) {
+if (T) {
 
 setwd("/home/wviechtb/work/meta_analysis/metadat/repo")
 prep_data()
