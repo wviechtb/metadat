f044d8f971a4f19661693c338469d148c2eac168
Wolfgang Viechtbauer <wvb@wvbauer.com>
2019-04-09 02:05:11 +0200

    Quick update to keep the repo up-to-date.

--- prep_data.r
+++ prep_data.r
@@ -1,11 +1,63 @@
-rfiles <- list.files(path="data-raw", pattern="[.r]$")
-rfiles <- rfiles[rfiles != "prep_data.r"]
-#rfiles
+prep_data <- function(rebuild=FALSE, overwrite=NULL) {
 
-setwd("data-raw")
+   ### list all .r/.R files in the 'data-raw' directory
+   rfiles <- list.files(path="data-raw", pattern="[.][rR]$")
+   #rfiles <- rfiles[rfiles != "prep_data.r"]
+   #rfiles
 
-for (i in 1:length(rfiles)) {
-   source(rfiles[i])
+   ### change working directory to 'data-raw'
+   setwd("data-raw")
+
+   ### change working directory back to root on exit
+   on.exit(setwd(".."))
+
+   cat("File", paste0(rep(" ", max(nchar(rfiles))-4), collapse=""), "New", "Build", "\n")
+   cat(paste0(rep("-", max(nchar(rfiles))), collapse=""), "-----------", "\n", sep="")
+
+   ### source data preparation scripts
+   for (i in seq_along(rfiles)) {
+
+      ### get 'root' name of the data preparation script
+      root <- substr(rfiles[i], 1, nchar(rfiles[i])-2)
+
+      ### list all .rda files in the 'data' directory
+      ### TODO: do we need to do this within the loop?
+      rda.files <- list.files(path="../data/")
+
+      ### grep for all root.<anything>.rda files
+      rda.files <- grep(paste0("^", root, ".*.rda"), rda.files, value=TRUE)
+
+      ### check if .rda file(s) exists
+      rda.exists <- ifelse(length(rda.files) > 0, TRUE, FALSE)
+
+      cat(rfiles[i], paste0(rep(" ", max(nchar(rfiles))-nchar(rfiles[i])), collapse=""), ifelse(!rda.exists, "T   ", "F   "))
+
+      ### if rebuild=TRUE or if file does not exist, process data preparation script
+      if (rebuild || !rda.exists) {
+         cat("T")
+         source(rfiles[i])
+      } else {
+         cat("F")
+      }
+
+      #genrd(root, rda.files)
+
+      cat("\n")
+
+   }
+
+   ### make sure every .rda file in 'data' is in lower case
+   #for (i in seq_along(rfiles)) {
+   #
+   #}
+
 }
 
-setwd("..")
+### this is just for testing purposes; ignore
+
+if (F) {
+
+setwd("/home/wviechtb/work/meta_analysis/metadat/repo")
+prep_data()
+
+}
