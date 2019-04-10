f3708e708c3791932f42f35cf06e096ce15d804b
Daniel Noble <daniel.wa.noble@gmail.com>
2019-04-10 11:10:59 +1000

    Created data search function. Also edited concepts in one datafile

---
+++ R/dat_search.r
@@ -0,0 +1,20 @@
+
+dat_search <- function(pattern, fields = "concept"){
+	
+	# Get names of matching datasets
+	names <- utils::help.search(pattern, package = "metadat", fields = fields)$matches$Name
+	
+	# Get titles of matching datasets
+	title <- utils::help.search(pattern, package = "metadat", fields = fields)$matches$Name
+
+	cat("\nChoose the data file you would like to see:\n")
+	Q <- utils::menu(names)
+
+	# First explore the help file
+	help(names[Q])
+
+}
+
+#Examples
+#dat_search("education")
+#dat_search("ecology")
