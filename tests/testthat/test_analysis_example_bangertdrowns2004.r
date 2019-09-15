4eed30b61f9f1337fa5217177ed622ed191c7f00
W. Kyle Hamilton <kyle.hamilton@gmail.com>
2019-09-15 15:23:02 -0700

    Updated data testing
    Data testing now checks md5 hash, data dimensions, and data types.

--- tests/testthat/test_analysis_example_bangertdrowns2004.r
+++ tests/testthat/test_analysis_example_bangertdrowns2004.r
@@ -1,6 +1,8 @@
-context("test-dat.bangertdrowns2004.R")
-library(metadat)
+context("Checking analysis example: bangertdrowns2004")
+
 library(metafor)
+source("tolerances.r") # read in tolerances
+
 
 ### copy data into 'dat' and examine data
 dat <- dat.bangertdrowns2004
@@ -14,7 +16,7 @@ test_that("Returns rma object", {
   expect_is(res, "rma.uni")
 })
 
-test_that("Checks rma results", {
+test_that("results are correct for the random-effects model.", {
   expect_identical(round(res$k, digits = 2), 48)
   expect_identical(round(as.numeric(res$beta[,1]), digits = 7), 0.2219296)
   expect_identical(round(as.numeric(res$tau2), digits = 7), 0.0499411)
--- tests/testthat/test_analysis_example_barone2019.r
+++ tests/testthat/test_analysis_example_barone2019.r
@@ -1,9 +1,10 @@
-context("test-dat.barone2019.R")
+context("Checking analysis example: barone2019")
+
 library(metadat)
 library(metafor)
 
 ## Make copy of dataset
-dat <- dat.barone2019
+dat <- metadat::dat.barone2019
 
 # First model 
 res <- rma(yi=logit.p., sei = sei, data=dat, slab = paste(Author, Year)) 
@@ -13,7 +14,7 @@ test_that("Returns rma object", {
   expect_is(res, "rma.uni")
 })
 
-test_that("Checks rma results", {
+test_that("results are correct for the random-effects model.", {
   expect_identical(round(res$k, digits = 2), 56)
   expect_identical(round(as.numeric(res$beta[,1]), digits = 7), 0.5662584)
   expect_identical(round(as.numeric(res$tau2), digits = 7), 0.4949573)
