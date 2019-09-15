4e8afb92d5a225f33adf35e3a65b1b19f18c2363
W. Kyle Hamilton <kyle.hamilton@gmail.com>
2019-09-15 15:33:58 -0700

    Added data label check
    forgot to do this

--- tests/testthat/test_data_barone2019.R
+++ tests/testthat/test_data_barone2019.R
@@ -16,6 +16,31 @@ test_that('data dimensions are correct', {
   expect_equal(nrow(dat), 56)
 })
 
+test_that(' data labels are correct', {
+  expect_equal(names(dat)[1], "id")
+  expect_equal(names(dat)[2], "Author")
+  expect_equal(names(dat)[3], "Year")
+  expect_equal(names(dat)[4], "Paradigm")
+  expect_equal(names(dat)[5], "Measure")
+  expect_equal(names(dat)[6], "Age_months")
+  expect_equal(names(dat)[7], "n")
+  expect_equal(names(dat)[8], "Dropped_participants")
+  expect_equal(names(dat)[9], "Dropped_participants_corrected")
+  expect_equal(names(dat)[10], "Familiarization")
+  expect_equal(names(dat)[11], "Belief")
+  expect_equal(names(dat)[12], "Agent")
+  expect_equal(names(dat)[13], "Target_object_Presence")
+  expect_equal(names(dat)[14], "Object_movements")
+  expect_equal(names(dat)[15], "Motive_transformation")
+  expect_equal(names(dat)[16], "Salience_of_agents_MS")
+  expect_equal(names(dat)[17], "Interaction")
+  expect_equal(names(dat)[18], "Design")
+  expect_equal(names(dat)[19], "Test_trials")
+  expect_equal(names(dat)[20], "logit.p.")
+  expect_equal(names(dat)[21], "vlogit.p.")
+  expect_equal(names(dat)[22], "sei")
+})
+
 test_that('data types are correct', {
   expect_is(dat, 'data.frame')
   expect_is(dat$id, 'character')
@@ -41,3 +66,4 @@ test_that('data types are correct', {
   expect_is(dat$vlogit.p., 'numeric')
   expect_is(dat$sei, 'numeric')
 })
+
