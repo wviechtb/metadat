ebff5247c34744904b35c31846bc21ae041ec8e2
W. Kyle Hamilton <kyle.hamilton@gmail.com>
2020-01-15 11:57:20 -0800

    Start of Sala 2019

---
+++ data-raw/dat.sala2019.r
@@ -0,0 +1,7 @@
+# Load
+# This csv file was generated from an excel file which was downloaded from https://osf.io/h9sjk/ on Jan 15, 2020
+# Paper and link to the data can be found here https://www.collabra.org/articles/10.1525/collabra.203/#additional-files
+dat.sala2019 <- read.csv("data-raw/dat.sala2019.csv", header=TRUE,  sep=";", stringsAsFactors=FALSE)
+
+# Output
+save(dat.sala2019, file="data/dat.sala2019.rda", ascii=FALSE)
