9e0fb0029e2de8d9b96c9d7b79c26b0e385a7a62
W. Kyle Hamilton <kyle.hamilton@gmail.com>
2020-05-21 12:04:36 -0700

    Added second dataset, fixed typo and hashtable error
    olfactory identification data added

--- tests/testthat/test_data_sorokowski2019.R
+++ tests/testthat/test_data_sorokowski2019.R
@@ -9,9 +9,9 @@ test_that("checks data md5 hash", {
   expect_match(digest(metadat::dat.sorokowski2019.1, algo = "md5"), hashTable$sorokowski2019.1)
 })
 
-# test_that("checks data md5 hash", {
-#   expect_match(digest(metadat::dat.nakagawa2015.2, algo = "md5"), hashTable$nakagawa2015.2)
-# })
+test_that("checks data md5 hash", {
+  expect_match(digest(metadat::dat.sorokowski2019.2, algo = "md5"), hashTable$sorokowski2019.2)
+})
 # 
 # test_that("checks data md5 hash", {
 #   expect_match(digest(metadat::dat.nakagawa2015.3, algo = "md5"), hashTable$nakagawa2015.3)
