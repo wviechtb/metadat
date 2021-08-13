8b3c7ceff7d3038daae46291b974e8af807cb30c
W. Kyle Hamilton <kyle.hamilton@gmail.com>
2021-08-12 17:51:44 -0700

    added dat.axfors2021
    Fixed an error in the test for dat.hannum2020.

--- tests/testthat/test_dat.hannum2020.r
+++ tests/testthat/test_dat.hannum2020.r
@@ -3,5 +3,5 @@ context("Checking: dat.hannum2020")
 library(digest)
 
 test_that("md5 hash matches for the data", {
-   expect_match(digest(metadat::dat.hannum2020, algo="md5"), "b9c59303d60d42bd4d27f1035f866473")
+   expect_match(digest(metadat::dat.hannum2020, algo="md5"), "62d64a11d98c3497f17036c41e0a7fcc")
 })
