ebbecfb4c1747dcc8a06406f27ae602b9b278390
W. Kyle Hamilton <whamilton@ucmerced.edu>
2021-08-12 20:04:06 -0700

    Update test_dat.hannum2020.r

--- tests/testthat/test_dat.hannum2020.r
+++ tests/testthat/test_dat.hannum2020.r
@@ -3,5 +3,5 @@ context("Checking: dat.hannum2020")
 library(digest)
 
 test_that("md5 hash matches for the data", {
-   expect_match(digest(metadat::dat.hannum2020, algo="md5"), "62d64a11d98c3497f17036c41e0a7fcc")
+   expect_match(digest(metadat::dat.hannum2020, algo="md5"), "5ce2207a2f8acdc2109388a116e3f45b")
 })
