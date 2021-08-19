a97cca2e164a9f3a916624e46673b9efd19eccc3
Thomas White <thomas.white026@gmail.com>
2021-08-19 14:33:21 +1000

    remove some unneeded datasets

--- to_be_added/data-raw/dat.griffith2006.r
+++
@@ -1,5 +0,0 @@
-# Load
-dat.griffith2006 <- read.csv("data-raw/dat.griffith2006.csv", header=TRUE, stringsAsFactors=FALSE)
-
-# Output
-save(dat.griffith2006, file="data/dat.griffith2006.rda", ascii=FALSE)
