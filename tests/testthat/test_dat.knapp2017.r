23f3c2c001be772f73a2fbe3f9c15eceda467396
Wolfgang Viechtbauer <wvb@wvbauer.com>
2021-10-02 20:50:31 +0200

    More info added to dat.knapp2017.

--- tests/testthat/test_dat.knapp2017.r
+++ tests/testthat/test_dat.knapp2017.r
@@ -3,5 +3,5 @@ context("Checking: dat.knapp2017")
 library(digest)
 
 test_that("md5 hash matches for the data", {
-   expect_match(digest(metadat::dat.knapp2017, algo="md5"), "2a2a4d239a94eaad6721a14d4cd83a37")
+   expect_match(digest(metadat::dat.knapp2017, algo="md5"), "0d866a1328248f8c6458c5127c443e02")
 })
