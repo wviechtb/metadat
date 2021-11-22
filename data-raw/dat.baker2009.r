691805f9caf8e56e0cad47084388410f592349d4
Guido Schwarzer <sc@imbi.uni-freiburg.de>
2021-11-22 12:42:57 +0100

    Add dataset 'dat.baker2009'; fix filename 'dat.lehmann2018.r'

---
+++ data-raw/dat.baker2009.r
@@ -0,0 +1,9 @@
+# Baker WL, Baker EL, Coleman CI (2009).
+# Pharmacologic Treatments for Chronic Obstructive Pulmonary Disease:
+# A Mixed-Treatment Comparison Meta-analysis.
+# Pharmacotherapy: The Journal of Human Pharmacology and Drug
+# Therapy, 29, 891-905.
+
+
+dat.baker2009 <- read.csv("dat.baker2009.txt")
+save(dat.baker2009, file="data/dat.baker2009.rda")
