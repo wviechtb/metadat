c5883f07e1dc423588628ab3cad5e18a44499a0f
Guido Schwarzer <sc@imbi.uni-freiburg.de>
2021-12-23 18:44:31 +0100

    Added dat.dogliotti2014 and dat.stowe2010

---
+++ data-raw/dat.dogliotti2014.r
@@ -0,0 +1,7 @@
+# Dogliotti A, Paolasso E, Giugliano RP (2014).
+# Current and new oral antithrombotics in non-valvular atrial
+# fibrillation: a network meta-analysis of 79 808 patients.
+# Heart, 100, 396-405.
+
+dat.dogliotti2014 <- read.csv("data-raw/dat.dogliotti2014.txt")
+save(dat.dogliotti2014, file="data/dat.dogliotti2014.rda")
