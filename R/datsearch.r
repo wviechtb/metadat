56ff5db21bafe9080674fc16810a19ad457680a6
Wolfgang Viechtbauer <wvb@wvbauer.com>
2022-02-22 17:45:37 +0100

    Improved datsearch() function (single match opens directly, enter exits, can include commas to split up multiple patterns, continue prompting until exit).

--- R/datsearch.r
+++ R/datsearch.r
@@ -1,12 +1,31 @@
 datsearch <- function(pattern, concept=TRUE, matchall=TRUE, fixed=TRUE, pkgdown=FALSE) {
 
+   if (!is.character(pattern))
+      stop("Argument 'pattern' must be a string (vector).")
+
+   # for a concept term search or fixed-term full-text search, can specify a
+   # single pattern separated by "," or ";" or "and" or "AND" which will
+   # automatically be split into separate patterns
+   if (concept || fixed) {
+      pattern <- strsplit(pattern, ",", fixed=TRUE)
+      pattern <- unlist(pattern)
+      pattern <- strsplit(pattern, ";", fixed=TRUE)
+      pattern <- unlist(pattern)
+      pattern <- strsplit(pattern, " and ", fixed=TRUE)
+      pattern <- unlist(pattern)
+      pattern <- strsplit(pattern, " AND ", fixed=TRUE)
+      pattern <- unlist(pattern)
+      pattern <- trimws(pattern)
+   }
+
    # number of patterns specified
    n <- length(pattern)
 
-   matches <- list()
-
+   # immediately show warnings, so that the warning() above
    opwarn <- options(warn=1)
    on.exit(options(warn=opwarn$warn))
+
+   matches <- list()
 
    if (concept) {
 
@@ -55,52 +74,65 @@ datsearch <- function(pattern, concept=TRUE, matchall=TRUE, fixed=TRUE, pkgdown=
    names  <- matches$Name
    titles <- matches$Title
 
-   # shorten titles to avoid line wrapping
-   width <- options("width")$width
-   width.names  <- nchar(names)
-   width.titles <- nchar(titles)
-   max.width.titles <- width - max(width.names) - 10
-   titles <- ifelse(width.titles > max.width.titles, paste0(substr(titles, 1, max.width.titles), "..."), titles)
+   if (length(names) == 1L) {
 
-   # print names and titles of matches
-   cat("\n")
-   print(data.frame(name = names, title = titles), right=FALSE)
+      # if there is only one match, automatically select this match
+      sel <- 1
 
-   exit <- FALSE
-
-   while (TRUE) {
-
-      sel <- readline(prompt = "\nChoose the number of the dataset you would like to see (or 0 to exit): ")
-      sel <- suppressWarnings(round(as.numeric(sel)))
-
-      if (is.na(sel)) {
-         message("Must enter a dataset number.")
-         next
-      }
-
-      if (sel == 0) {
-         exit <- TRUE
-         break
-      }
-
-      if (sel < 1 || sel > length(titles)) {
-         message(paste0("Dataset number must be between 1 and ", length(titles), "."))
-         next
-      }
-
-      break
-
-   }
-
-   if (exit)
-      return(invisible())
-
-   # show the help file
-   if (pkgdown) {
-      url <- paste0("https://wviechtb.github.io/metadat/reference/", names[sel], ".html")
-      browseURL(url)
    } else {
-      help(names[sel], package="metadat")
+
+      # otherwise prompt user to select one of the matches
+
+      # shorten titles to avoid line wrapping
+      width <- options("width")$width
+      width.names  <- nchar(names)
+      width.titles <- nchar(titles)
+      max.width.titles <- width - max(width.names) - 10
+      titles <- ifelse(width.titles > max.width.titles, paste0(substr(titles, 1, max.width.titles), "..."), titles)
+
+      # print names and titles of matches
+      cat("\n")
+      print(data.frame(Name = names, Title = titles), right=FALSE)
+      cat("\n")
+
+      exit <- FALSE
+
+      while (TRUE) {
+
+         sel <- readline(prompt = "Choose the number of the dataset you would like to see (or <enter> to exit): ")
+
+         if (sel == "" || sel == "0") {
+            exit <- TRUE
+            break
+         }
+
+         sel <- suppressWarnings(round(as.numeric(sel)))
+
+         if (is.na(sel)) {
+            message("Must enter a dataset number.")
+            next
+         }
+
+         if (sel < 1 || sel > length(titles)) {
+            message(paste0("Dataset number must be between 1 and ", length(titles), "."))
+            next
+         }
+
+         #break
+
+         # show the help file
+         if (pkgdown) {
+            url <- paste0("https://wviechtb.github.io/metadat/reference/", names[sel], ".html")
+            browseURL(url)
+         } else {
+            print(help(names[sel], package="metadat"))
+         }
+
+      }
+
+      if (exit)
+         return(invisible())
+
    }
 
 }
