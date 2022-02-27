f6b9152407b2bd76b493a17232b3a51b72a75fe9
Wolfgang Viechtbauer <wvb@wvbauer.com>
2022-02-27 11:19:36 +0100

    Improved the shiny app so that all concept boxes are shown and some further minor simplifcations in the code. [ci skip]

--- shiny/shiny.r
+++ shiny/shiny.r
@@ -1,5 +1,3 @@
-# I cant get the targets part sorted out but perhaps @Kyle knows how to do this. 
-# But the search now shows the single methods and searches on them
 library(metadat)
 library(shiny)
 library(DT)
@@ -17,51 +15,54 @@ server <- shinyServer(function(input, output, session) {
    db <- db[grep("^dat[.]", names(db))]
 
    # metadata per Rd file
-   name <- lapply(db, tools:::.Rd_get_metadata, "name")
-   title <- lapply(db, tools:::.Rd_get_metadata, "title")
-   description <- lapply(db, tools:::.Rd_get_metadata, "description")
+   name        <- sapply(db, tools:::.Rd_get_metadata, "name")
+   title       <- sapply(db, tools:::.Rd_get_metadata, "title")
+   description <- sapply(db, tools:::.Rd_get_metadata, "description")
 
    # remove the mathjaxr loading stuff from the descriptions
-   description  <- lapply(description, function(x) strsplit(x, "\n\\if{html}", fixed=TRUE)[[1]][1])
-   concepts.raw <- lapply(db, tools:::.Rd_get_metadata, "concept")
+   description <- sapply(description, function(x) strsplit(x, "\n\\if{html}", fixed=TRUE)[[1]][1])
 
+   # some further substitutions due to use of \dQuote or \emph in the titles or descriptions
+   #z <- sapply(title, function(x) grepl("\n", x, fixed=TRUE)); z[z]
+   title <- sapply(title, function(x) gsub("\nlist(\"", "'", x=x, fixed=TRUE))
+   title <- sapply(title, function(x) gsub("\")\n", "'", x=x, fixed=TRUE))
+   description <- sapply(description, function(x) gsub("\nlist(\"", "'", x=x, fixed=TRUE))
+   description <- sapply(description, function(x) gsub("\")\n", "'", x=x, fixed=TRUE))
+
+   # all concept terms used so far split by fields, measures, and methods
    fields   <- c("alternative medicine", "attraction", "cardiology", "climate change", "covid-19", "criminology", "dentistry", "ecology", "education", "engineering", "epidemiology", "evolution", "human factors", "medicine", "memory", "obstetrics", "oncology", "persuasion", "primary care", "psychiatry", "psychology", "smoking", "social work", "sociology")
    measures <- c("correlation coefficients", "Cronbach's alpha", "hazard ratios", "incidence rates", "raw mean differences", "odds ratios", "proportions", "ratios of means", "raw means", "risk differences", "risk ratios", "(semi-)partial correlations", "standardized mean changes", "standardized mean differences")
    methods  <- c("cluster-robust inference", "component network meta-analysis", "cumulative meta-analysis", "diagnostic accuracy studies", "dose response models", "generalized linear models", "longitudinal models", "Mantel-Haenszel method", "meta-regression", "model checks", "multilevel models", "multivariate models", "network meta-analysis", "outliers", "Peto's method", "phylogeny", "publication bias", "reliability generalization", "single-arm studies", "spatial correlation")
    
-   fields   <- sapply(concepts.raw, function(x) paste0(fields[fields %in% x], collapse=", "))
-   measures <- sapply(concepts.raw, function(x) paste0(measures[measures %in% x], collapse=", "))
-   methods  <- sapply(concepts.raw, function(x) paste0(methods[methods %in% x], collapse=", "))
+   # for each dataset, collapse multiple terms within these categories down into a single comma-separated string
+   concepts <- sapply(db, tools:::.Rd_get_metadata, "concept")
+   fields   <- sapply(concepts, function(x) paste0(fields[fields %in% x], collapse=", "))
+   measures <- sapply(concepts, function(x) paste0(measures[measures %in% x], collapse=", "))
+   methods  <- sapply(concepts, function(x) paste0(methods[methods %in% x], collapse=", "))
    
-   dataExtracted <- as.data.frame(cbind(name, title, description, fields, measures, methods))
-   df <- dataExtracted
-   df$name <- paste0(
-      "<a href='",
-      "https://wviechtb.github.io/metadat/reference/",
-      name,
-      ".html",
-      "'>",
-      name,
-      "</a>"
-   )
-   
+   # create the data frame
+   df <- data.frame(name, title, description, fields, measures, methods)
+   df$name <- paste0("<a href='https://wviechtb.github.io/metadat/reference/", name, ".html'>", name, "</a>")
+   rownames(df) <- NULL
+
    options(DT.options = list(pageLength = 15))
 
    # output data table
    table_export <- DT::datatable(
       df,
       escape = FALSE,
-      extensions = c('Select', 'SearchPanes'),
-      selection = 'none',
+      extensions = c("Select", "SearchPanes"),
+      selection = "none",
+      colnames = c("Documentation", "Title", "Study Description", "Fields", "Measures", "Methods"),
       options = list(
          dom = "Pfrtip", 
          searchPanes = list(
             threshold = 1,
-            columns = list(6)
+            columns = 4:6
          ),
          columnDefs = list(
             list(
-               targets = 6,
+               targets = 4:6,
                render = JS(
                   "function (data, type, row) {",
                   "  if (type === 'sp') {",
