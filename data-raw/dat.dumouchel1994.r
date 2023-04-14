781d5aa5bb0ff232754e50157d4acb0dc250d890
Christian Roever <christian.roever@med.uni-goettingen.de>
2023-04-14 17:02:33 +0200

    added 'dat.dumouchel1994' data set

---
+++ data-raw/dat.dumouchel1994.r
@@ -0,0 +1,8 @@
+dat.dumouchel1994 <- read.csv("data-raw/dat.dumouchel1994.csv")
+
+for (i in 2:4)
+  dat.dumouchel1994[,i] <- factor(dat.dumouchel1994[,i],
+                                  levels=c("no","yes"))
+
+save("dat.dumouchel1994",
+     file="data/dat.dumouchel1994.rda")
