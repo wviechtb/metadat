a3833eb278ca75909a7ebf3865bd727677b5556b
Guido Schwarzer <guido.schwarzer@uniklinik-freiburg.de>
2024-01-22 15:27:48 +0100

    Add some datasets used in Schwarzer et al. (2015)

---
+++ data-raw/dat.bassler2004.r
@@ -0,0 +1,7 @@
+# Bassler D, Mitra A, Ducharme FM, J Forster J, Schwarzer G (2004).
+# Ketotifen alone or as additional medication for
+# long-term control of asthma and wheeze in children.
+# Cochrane Database of Systematic Reviews, CD001384.
+
+dat.bassler2004 <- read.csv("data-raw/dat.bassler2004.txt")
+save(dat.bassler2004, file = "data/dat.bassler2004.rda")
