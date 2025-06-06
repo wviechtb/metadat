ec0681cca501da79cc23592a01b957c6d537345e
Guido Schwarzer <guido.schwarzer@uniklinik-freiburg.de>
2025-06-06 20:28:52 +0200

    Change md5sum in test for 'dat.linde2016'

--- tests/testthat/test_dat.linde2016.r
+++ tests/testthat/test_dat.linde2016.r
@@ -3,5 +3,5 @@ context("Checking: dat.linde2016")
 library(digest)
 
 test_that("md5 hash matches for the data", {
-   expect_match(digest(metadat::dat.linde2016, algo="md5"), "33020cfa2aed4478bac3364b0dd3a9dd")
+   expect_match(digest(metadat::dat.linde2016, algo="md5"), "5b568ccabbe79a4c4cc9faa50bf3ba8a")
 })
