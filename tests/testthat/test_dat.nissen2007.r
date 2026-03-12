ad2dae88ab684db236ad91aa2fb65cc2ae51328a
Wolfgang Viechtbauer <wvb@wvbauer.com>
2026-03-12 10:27:21 +0100

    Updated version of dat.nissen2007.

--- tests/testthat/test_dat.nissen2007.r
+++ tests/testthat/test_dat.nissen2007.r
@@ -3,5 +3,5 @@ context("Checking: dat.nissen2007")
 library(digest)
 
 test_that("md5 hash matches for the data", {
-   expect_match(digest(metadat::dat.nissen2007, algo="md5"), "b4d17e9eecf12f3a1de08c4823c25250")
+   expect_match(digest(metadat::dat.nissen2007, algo="md5"), "8cf5635d90038997a3099c66ba205e83")
 })
